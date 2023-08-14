2ca3ed8c66635160566ff6b086de728b86d079c5
yonghyeun <ttddcc119@naver.com>
2023-08-14 16:08:24 +0900

    Yonganalysis __version__1

---
+++ YongAnalysis.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+from scipy import stats
+from pprint import pprint
+import sys
+from colorama import Style, Fore
+import warnings
+
+
+class DataExploratioin:
+    '''
+    데이터 탐색 시 사용 가능한 Class 
+
+    기존 존재하는 프레임워크들을 이용하여 자주 이용하는 프레임워크들을 활용하여 나만의 분석 툴을 만들려고 함 
+
+    데이터 요약, 결측값 처리 등의 내용이 담겨있는 class 
+    '''
+
+    def __init__(self, data):
+        self.data = data
+
+    def summarize(self):
+        '''
+        데이터를 초창기에 요약해주는 method
+        '''
+
+        cols = self.data.columns
+
+        size = round(sys.getsizeof(self.data) / 1024 ** 2, 2)
+
+        print(f'data size : {size}MB')
+
+        self.result = pd.DataFrame()
+
+        self.result['Dtype'] = self.data.dtypes.values
+        self.result['Count'] = self.data.count().values
+        self.result['Nunique'] = self.data.nunique().values
+        self.result['Missing value'] = self.data.isna().sum().values
+        self.result['Missing %'] = [str(round(
+            missing / len(self.data), 2) * 100) + '%' for missing in self.result['Missing value']]
+        self.result['Most Freq Value'] = self.data.mode().iloc[0].values
+
+        freq_prop = []
+
+        for i, col in enumerate(cols):
+
+            raw_data = self.data.loc[~self.data[col].isna(), col]
+            freq_value = self.result['Most Freq Value'].iloc[i]
+
+            prop = np.mean(
+                np.array(self.data.loc[~self.data[col].isna(), col]
+                         == self.result['Most Freq Value'].iloc[i])
+            )
+
+            prop_str = str(round(np.mean(prop) * 100, 1)) + '%'
+
+            if prop_str == 'nan%':
+                freq_prop.append(self.result['Missing %'].iloc[i])
+            else:
+                freq_prop.append(prop_str)
+
+        self.result['Most Freq Value %'] = freq_prop
+
+        self.result['Min'] = self.data.describe(include='all').T['min'].values
+        self.result['Max'] = self.data.describe(include='all').T['max'].values
+        self.result['Mean'] = self.data.describe(
+            include='all').T['mean'].values
+        self.result['Median'] = self.data.describe(
+            include='all').T['50%'].values
+
+        self.result = self.result.set_index(cols)
+
+        self.result = self.result.fillna('-')
+
+        return self.result, freq_prop
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yonganalysis",  # 소문자 영단어
+    version="0.0.1",
+    author="Yonghyeun Choi",  # ex) Sunkyeong Lee
+    author_email="ttddcc119@naver.com",
+    description="Analysis frame work",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yonghyeun/YongAnalysis.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
